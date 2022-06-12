68e5418f1a24e05672e42b85a3a85afa7fb11552
bsao17 <declic62@gmail.com>
2022-06-12 11:22:57 +0200

    use useParams hook

--- src/routes/Invoice.tsx
+++ src/routes/Invoice.tsx
@@ -1,3 +1,20 @@
+import { useParams } from "react-router-dom";
+import { getInvoice } from "../data";
+
 export default function Invoice() {
-    return <h2>Invoice #???</h2>;
-  }
+  let params = useParams();
+  //@ts-ignore
+  let invoice: string = getInvoice(parseInt(params.invoiceId, 10));
+  return (
+    <main style={{ padding: "1rem" }}>
+        {/**@ts-ignore */}
+      <h2>Total Due: {invoice.amount}</h2>
+      <p>
+          {/**@ts-ignore */}
+        {invoice.name}: {invoice.number}
+      </p>
+      {/**@ts-ignore */}
+      <p>Due Date: {invoice.due}</p>
+    </main>
+  );
+}
